ed86461b2c40716d50234bb333ff788360bbf01e
matebcn <>
2018-04-20 13:55:40 +0200

    SQL: creación de tabla y datos de demostración

---
+++ postgresql/sqlNoSQL.sql
@@ -0,0 +1,18 @@
+https://www.compose.com/articles/faster-operations-with-the-jsonb-data-type-in-postgresql/
+
+CREATE TABLE alumnos (  
+  id serial NOT NULL,
+  nombre text,
+  apellido text,
+  mail text,
+  active int,
+  caracteristicas jsonb
+);
+
+
+INSERT INTO alumnos VALUES (1, 'Luis', 'Vecino', 'vecino@luis.gll', 1, '{"pelo":"calvo","ojos":"marrones", "nacionalidad": "gallego"}');  
+INSERT INTO alumnos VALUES (2, 'Daniel', 'Pare', 'pare@daniel.cat', 1,  '{"pelo":"castaño","ojos":"marrones", "nacionalidad": "cat"}');  
+INSERT INTO alumnos VALUES (3, 'Ricart', 'Valira', 'valira@ricart.es',1,'{"pelo":"canoso","ojos":"verdes", "nacionalidad": "cat"}');  
+INSERT INTO alumnos VALUES (4, 'Miguel', 'Mate', 'mate@miguel.ovn',  1, '{"pelo":"moreno","ojos":"celestes", "nacionalidad": "ovni"}');  
+
+
